1ff045adc88b3b240ad1c8d6250574f688e24aa7
Moises Perez <perezmoises132@gmail.com>
2023-06-27 08:02:40 -0600

    Fixed eslint error on PageNotFound

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const PageNotFound = () => (
+  // eslint-disable-next-line react/no-unescaped-entities
   <h1> This is not the 404 page you're looking for. </h1>
 );
 
